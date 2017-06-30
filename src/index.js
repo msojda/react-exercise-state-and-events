a82dd810e67d8df370e85ebbdecce6a096e624d7
Mateusz Sojda <mateusz.sojda@worldfirst.com>
2017-06-30 14:13:18 +0100

    Completed part 1.

--- src/index.js
+++ src/index.js
@@ -2,20 +2,36 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 class ShoppingList extends React.Component {
+  itemInput;
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: ['Bread', 'Apple', 'Banana']
+    };
+  }
+
+  addProduct(event) {
+    event.preventDefault();
+
+    this.setState({
+      products: [...this.state.products, this.itemInput.value]
+    });
+  }
+
   render() {
     return (
       <div>
         <h2>Shopping list</h2>
 
         <form>
-          <input type="text" placeholder="Item name..." />
-          <button type="submit">Add</button>
+          <input type="text" placeholder="Item name..." ref={(input) => this.itemInput = input} />
+          <button type="submit" onClick={(event) => this.addProduct(event)}>Add</button>
         </form>
 
         <ul>
-          <li>Bread</li>
-          <li>Oranges</li>
-          <li>Olive oil</li>
+          {this.state.products.map((product, index) => <li key={index}>{product}</li>)}
         </ul>
       </div>
     )
